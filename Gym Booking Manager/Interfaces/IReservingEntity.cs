9371a291ea20c8da11ecb3398c5011081a81919f
victo <victor.rasmussen.vr@gmail.com>
2023-02-06 15:46:35 +0100

    Add Intreface class IReservingEntity = no crash when compiling!!

---
+++ Gym Booking Manager/Interfaces/IReservingEntity.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Gym_Booking_Manager
+{
+    // Just a test.....so this may not last!! 
+    public interface IReservingEntity
+    {
+        //What should this Interface do?
+        //Only the brave will find out!
+    }
+}
