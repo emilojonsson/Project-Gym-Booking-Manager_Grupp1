cb1e4ece25d3c08480d146b61c44428a601f316b
emil <emil.o.jonsson@gmail.com>
2023-02-13 10:11:18 +0100

    .

--- Gym Booking Manager/Calendar.cs
+++ Gym Booking Manager/Calendar.cs
@@ -21,10 +21,21 @@ namespace Gym_Booking_Manager
     internal class Calendar
     {
         [DataMember]
-        private readonly List<Reservation> reservations;        [DataMember]        private DateTime dateTime; // Becca skrev in tillfälligt        public Calendar() //this is to be used when creating an item for the first time (space, trainer, equipment)
+        private readonly List<Reservation> reservations;
+        [DataMember]
+        private DateTime dateTime; // Becca skrev in tillfälligt
+
+        public Calendar() //this is to be used when creating an item for the first time (space, trainer, equipment)
         {
             this.reservations = new List<Reservation>();
-        }        public Calendar(DateTime dateTime)        {            this.dateTime = dateTime;        }        public Calendar(DateTime timeSlot, ReservingEntity Owner) //this is to be used when creating an Activity
+        }
+
+        public Calendar(DateTime dateTime)
+        {
+            this.dateTime = dateTime;
+        }
+
+        public Calendar(DateTime timeSlot, ReservingEntity Owner) //this is to be used when creating an Activity
         {
             this.reservations = new List<Reservation> {new Reservation(Owner, timeSlot)};
         }
