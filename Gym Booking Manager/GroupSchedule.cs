b8e9c456a49a31c9335a050349cd4d87034957b7
Rebecca Borgqvist <rebeccaborgqvist@gmail.com>
2023-02-14 13:08:28 +0100

    Moved SignUp from Activity to Groupschedule

--- Gym Booking Manager/GroupSchedule.cs
+++ Gym Booking Manager/GroupSchedule.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Diagnostics;using System.Linq;
-=======
-using System.Diagnostics;
-using System.Linq;
->>>>>>> fc9a5c223286db3e07f152ed92befa33d706e21d
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -122,7 +117,7 @@ namespace Gym_Booking_Manager
         }
 
 
-        public void RemoveActivity(ReservingEntity user)
+        public void RemoveActivity(ReservingEntity user, string activityID)
         {
             if (user.status == "Member")
             {
@@ -158,26 +153,36 @@ namespace Gym_Booking_Manager
                         if (count == answerInt)
                         {
                             activity.participants.Remove(user);
-                            Console.WriteLine($"Användaren {user.name} är nu avanmäld från {activity.activityDetails}");
+                            Console.WriteLine($"Användaren {user.name} was removed from activity {activity.activityID}");
                             break;
                         }
                     }
                 }
+
+                //if (activity.activityID == activityID)
+                //{
+                //    activity.participants.Remove(user);
+                //    Console.WriteLine($"The User {user.name} was removed from activity {activity.activityID}");
+                //    break;
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"The User was not found in the schedule.");
+                //}
             }
             else if (user.status == "Staff")
             {
-                Console.WriteLine("Nedan listas de gruppaktiviteter som finns i schemat:");
-                int count = 1;
-                foreach (Activity activity in activities)
-                {
-                    Console.WriteLine($"{count}. {activity.activityDetails}, starttid: {activity.timeSlot.reservations[0].startTime}"); //for now an activity can only have one reservation, but beware of future changes...
-                    count++;
-                }
-                Console.WriteLine("Ange den gruppaktivitet som ska tas bort:");
-                int answerInt = int.Parse(Console.ReadLine()) - 1;
-
-                Console.WriteLine($"Gruppaktiviteten {activities[answerInt].activityDetails} är nu borttaget från schemat");
-                activities.RemoveAt(answerInt);
+                //if (activity.activityID == activityID)
+                //{
+                //    activities.Remove(activity);
+                //    Console.WriteLine($"The activity with ID {activityID} has been removed from the schedule.");
+                //    // Here we need a method so that the activity gets removed from the Database!!
+                //    break;
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"The activity with ID {activityID} was not found in the schedule.");
+                //}
             }
         }        public void SignUp(ReservingEntity user, DatabaseTemp data1)
         {
